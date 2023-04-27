f3e638eaa7619af091a268ed2c0783bcc79e3d11
JimS <jimsun6428@gmail.com>
2023-04-27 15:55:31 +0800

    dark mode, icon, handle check chapter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,133 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { TiTick } from "react-icons/ti";
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
 
-const inter = Inter({ subsets: ['latin'] })
+const Chapter = ({
+  item,
+  handleCheck,
+}: {
+  item: Chapter;
+  handleCheck: (id: string) => void;
+}) => {
+  return (
+    <div>
+      <input
+        className="hidden"
+        type="checkbox"
+        id={item.id}
+        checked={item.checked}
+        onChange={() => handleCheck(item.id)}
+      />
+      <label
+        className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-500"
+        htmlFor={item.id}
+      >
+        <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-slate-300 rounded-full">
+          <TiTick />
+        </span>
+        <span className="ml-4 text-sm">{item.name}</span>
+      </label>
+    </div>
+  );
+};
+
+const ThemeButton = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <button
+      onClick={() =>
+        currentTheme == "dark" ? setTheme("light") : setTheme("dark")
+      }
+      className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-2 py-2 rounded-lg text-xl"
+    >
+      {currentTheme === "dark" && <BsSunFill />}
+      {currentTheme !== "dark" && <BsMoonFill />}
+    </button>
+  );
+};
+
+type Chapter = {
+  name: string;
+  id: string;
+  checked: boolean;
+  val: number;
+};
+
+const getPorgress = (arr: Chapter[]): number => {
+  const sum = arr.reduce((prev, curr) => prev + curr.val, 0);
+  const finish = arr
+    .filter((item) => item.checked)
+    .reduce((prev, curr) => prev + curr.val, 0);
+  return finish === 0 ? 0 : Math.floor((finish / sum) * 100);
+};
 
 export default function Home() {
+  const [chapters, setChapters] = useState(testChapter);
+
+  if (!chapters) {
+    return <div>Loading...</div>;
+  }
+
+  // calculate progress in component
+  const progress = getPorgress(chapters);
+
+  // check a chapter
+  const handleCheck = (id: string) => {
+    setChapters(() =>
+      chapters.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            checked: !item.checked,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center pt-24`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="absolute top-0 right-0 p-4">
+        <ThemeButton />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-gray-200 dark:bg-gray-300 rounded-full w-52 h-4">
+        <div
+          className="bg-blue-500 rounded-full h-4"
+          style={{ width: `${progress}%` }}
+        ></div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col mt-5 space-y-3 w-52">
+        {chapters.map((item) => (
+          <Chapter key={item.id} item={item} handleCheck={handleCheck} />
+        ))}
       </div>
     </main>
-  )
+  );
 }
+
+const testChapter: Chapter[] = [
+  {
+    name: "chapter1",
+    id: "dqodjoe",
+    checked: true,
+    val: 11,
+  },
+  {
+    name: "chapter2",
+    id: "ewjidhq",
+    checked: false,
+    val: 23,
+  },
+  {
+    name: "chapter3",
+    id: "ewjid2ehq",
+    checked: false,
+    val: 45,
+  },
+];
