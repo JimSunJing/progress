9914cf77cb9a917136efd0c2647bcbdd1150aa66
JimS <jimsun6428@gmail.com>
2023-05-04 15:00:01 +0800

    delete button, create input ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import { useTheme } from "next-themes";
 import { useState } from "react";
-import { TiTick } from "react-icons/ti";
+import { TiDeleteOutline, TiTick } from "react-icons/ti";
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
+import { IoAdd } from "react-icons/io5";
+import { FcCancel } from "react-icons/fc";
 
 const Chapter = ({
   item,
   handleCheck,
+  handleDelete,
 }: {
   item: Chapter;
   handleCheck: (id: string) => void;
+  handleDelete: (id: string) => void;
 }) => {
   return (
     <div>
@@ -26,8 +30,48 @@ const Chapter = ({
         <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-slate-300 rounded-full">
           <TiTick />
         </span>
-        <span className="ml-4 text-sm">{item.name}</span>
+        <span className="ml-4 text-sm flex-grow">{item.name}</span>
+        <span
+          className="text-xl text-slate-200 hover:text-slate-600"
+          onClick={(e) => {
+            e.stopPropagation();
+            handleDelete(item.id);
+          }}
+        >
+          <TiDeleteOutline />
+        </span>
       </label>
+    </div>
+  );
+};
+
+const NewChapter = () => {
+  const [onWrite, setOnWrite] = useState(false);
+
+  return (
+    <div className="flex mt-3 w-full">
+      {!onWrite && (
+        <div
+          className="text-slate-200 hover:text-slate-600 text-2xl hover:bg-slate-50 flex-grow px-2 py-1 rounded-md"
+          onClick={() => setOnWrite(true)}
+        >
+          <IoAdd />
+        </div>
+      )}
+      {onWrite && (
+        <>
+          <div className="relative w-full">
+            <input
+              type="text"
+              id="new-chapter-name"
+              className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <span className="text-2xl absolute right-2 top-1">
+              <FcCancel onClick={() => setOnWrite(false)} />
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
@@ -76,8 +120,8 @@ export default function Home() {
 
   // check a chapter
   const handleCheck = (id: string) => {
-    setChapters(() =>
-      chapters.map((item) => {
+    setChapters((prev) =>
+      prev.map((item) => {
         if (item.id === id) {
           return {
             ...item,
@@ -87,6 +131,10 @@ export default function Home() {
         return item;
       })
     );
+  };
+
+  const handleDelete = (id: string) => {
+    setChapters((prev) => prev.filter((item) => item.id !== id));
   };
 
   return (
@@ -104,8 +152,14 @@ export default function Home() {
       </div>
       <div className="flex flex-col mt-5 space-y-3 w-52">
         {chapters.map((item) => (
-          <Chapter key={item.id} item={item} handleCheck={handleCheck} />
+          <Chapter
+            key={item.id}
+            item={item}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         ))}
+        <NewChapter />
       </div>
     </main>
   );
